892db85bcf2761814cb179c5454fda2531664fb9
hexagon06 <jasper.saaltink@gmail.com>
2022-03-26 13:45:59 +0100

    refactoring to generic API

--- src/api/typed/creatureApi.ts
+++ src/api/typed/creatureApi.ts
@@ -2,15 +2,19 @@ import { userStore } from '@/store'
 import { UserCreatureData } from '@/types'
 import { Creature } from '@/types/creatures'
 import { collection, deleteField, getDocs, query, where } from 'firebase/firestore'
-import { firebaseClient } from './firebaseClient'
-import { FirestoreAcces } from './firestoreAccess'
+import { firebaseClient } from '../firebaseClient'
+import { FirestoreAcces } from '../firestoreAccess'
+import { Api } from '../genericApi'
 
 const CREATURE_COLLECTION = 'creatures'
 
-export async function getCreature (id: string): Promise<Creature | undefined> {
-  try {
-    const firestore = new FirestoreAcces<Creature>(firebaseClient.store, CREATURE_COLLECTION)
-    const creature = await firestore.getById(id)
+class CreatureApi extends Api<Creature> {
+  constructor() {
+    super(CREATURE_COLLECTION)
+  }
+
+  async get (id: string) {
+    const creature = await super.get(id)
 
     const colRef = collection(firebaseClient.store, `${CREATURE_COLLECTION}/${id}/userData`)
     const q = query(colRef, where('userId', '==', userStore.state.currentUser?.uid))
@@ -28,37 +32,24 @@ export async function getCreature (id: string): Promise<Creature | undefined> {
         id: userData.docs[0].id
       }
     }
+
     return creature
-  } catch (e) {
-    console.error(e)
   }
-}
 
-export async function createCreature (creature: Creature): Promise<string> {
-  try {
-    const firestore = new FirestoreAcces<Creature>(firebaseClient.store, CREATURE_COLLECTION)
-    const id = await firestore.add(creature)
-
+  async create (creature: Creature) {
+    const id = await super.create(creature)
     if (creature.userData) {
       const userDataStore = new FirestoreAcces<UserCreatureData>(firebaseClient.store, `${CREATURE_COLLECTION}/${id}/userData`)
       await userDataStore.add(creature.userData)
     }
-
     return id
-  } catch (e) {
-    console.error(e)
-    throw new Error('failed to create')
   }
-}
 
-export async function updateCreature (creature: Creature): Promise<void> {
-  try {
-    const firestore = new FirestoreAcces<Creature>(firebaseClient.store, CREATURE_COLLECTION)
-
+  async update (creature: Creature) {
     const entity = { ...creature, userData: deleteField() } as any
     const { id } = creature
 
-    await firestore.update(entity)
+    await super.update(entity)
 
     if (creature.userData) {
       const userDataStore = new FirestoreAcces<UserCreatureData>(firebaseClient.store, `${CREATURE_COLLECTION}/${id}/userData`)
@@ -70,8 +61,7 @@ export async function updateCreature (creature: Creature): Promise<void> {
       }
 
     }
-  } catch (e) {
-    console.error(e)
-    throw new Error('failed to update')
   }
 }
+
+export const creatureApi = new CreatureApi()
