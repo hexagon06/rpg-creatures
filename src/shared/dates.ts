92f9ac2b94a64587b2e74f09fd91b4a34d2e56fb
hexagon06 <jasper.saaltink@gmail.com>
2022-04-20 18:34:55 +0200

    fixing cr4eated time for items not indexes

--- src/shared/dates.ts
+++ src/shared/dates.ts
@@ -7,14 +7,16 @@ export function setInitialDates<T extends DatedItem> (item: T): T {
   return {
     ...item,
     created,
-    lastEdited
+    lastEdited,
   } as T
 }
 
 export function setEditedDate<T extends DatedItem> (item: T): T {
+  const created = item.created ? item.created : now()
   const lastEdited = now()
   return {
     ...item,
-    lastEdited
+    created,
+    lastEdited,
   } as T
 }
