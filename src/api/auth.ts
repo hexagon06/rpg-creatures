6db7e5fda91bc34803d25cc029ba749a72ea592d
hexagon06 <jasper.saaltink@gmail.com>
2022-05-05 15:32:44 +0200

    login with google or password-email

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,23 @@
-import { getAuth, connectAuthEmulator, signInWithEmailAndPassword, UserCredential, signOut as signAuthOut } from "firebase/auth"
+import { getAuth, connectAuthEmulator, signInWithEmailAndPassword, UserCredential, signOut as signAuthOut, GoogleAuthProvider, EmailAuthProvider } from "firebase/auth"
+import * as firebaseui from "firebaseui"
 
 export const auth = getAuth()
 if (process.env.NODE_ENV === 'development') {
   connectAuthEmulator(auth, "http://localhost:9099")
 }
 
-export async function signIn (email: string, password: string): Promise<UserCredential> {
-  return await signInWithEmailAndPassword(auth, email, password)
+const ui = new firebaseui.auth.AuthUI(auth)
+export function signIn (): void {
+  ui.start('#firebaseui-auth-container', {
+    signInFlow: 'popup',
+    signInOptions: [
+      // List of OAuth providers supported.
+      GoogleAuthProvider.PROVIDER_ID,
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    signInSuccessUrl: '/',
+    // Other config options...
+  })
 }
 
 export async function signOut (): Promise<void> {
